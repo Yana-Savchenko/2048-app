94790372c2fea8e2a2c03704e27d796a9cb3462e
Yana Savchenko <y.s.10.06.13@gmail.com>
2018-10-02 15:39:53 +0300

    add simple render

--- script/js.js
+++ script/js.js
@@ -1,17 +1,37 @@
 $(document).ready(function () {
     let gameSpace = [];
-    console.log(random());
+    initialize();
     function initialize() {
-    
-    }
-    function random() {
-        const newNumber = Math.round(0.5 + Math.random() * 10);
-        if (newNumber === 4) {
-            return 4;
+        for (let i = 0; i < 4; i++) {
+            gameSpace[i] = new Array();
+            for (var j = 0; j < 4; j++) {
+                gameSpace[i][j] = 0;
+            }
         }
-        return 2;
+        const startNumber = random(1, 10) === 4 ? 4 : 2;
+        const row = random(0, 3);
+        const column = random(0, 3);
+        gameSpace[row][column] = startNumber;
+        render();
+    }
+
+    // return 2 with probability 90% or 4 with probabiliti 10%;
+    function random(min, max) {
+        return Math.round(min - 0.5 + Math.random() * (max - min + 1));
     }
     function render() {
+      let cells = document.createDocumentFragment();
+      for (let i = 0; i < 4; i++) {
+        for (var j = 0; j < 4; j++) {
+            let cell = document.createElement('div');
+            $(cell).addClass("cell");
+            if(gameSpace[i][j]) {
+              $(cell).text(gameSpace[i][j]);
+            }
+            $(cells).append(cell);
+            $("#wrapper").append(cells);
+        }
 
+    }
     }
 });
